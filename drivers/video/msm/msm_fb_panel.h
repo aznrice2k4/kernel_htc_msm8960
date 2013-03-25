61ccfc3567b9b9fc67a69d97f04e90cca7ef4daa
aznrice2k4 <aznrice2k4@gmail.com>
2013-03-25 12:21:54 -0500

    msm_fb : display : Change fps level dynamically. This change lets the user modify the fps level dynamically using sysfs interface.
    Change-Id: Ie99bbca5ec1a0b5ff5fa3570874b254e3fb7235c
    Signed-off-by: Vishnuvardhan Prodduturi <vproddut@codeaurora.org>

--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -184,6 +184,8 @@ struct msm_fb_panel_data {
 	int (*power_ctrl) (boolean enable);
 	struct platform_device *next;
 	int (*clk_func) (int enable);
+	int (*fps_level_change) (struct platform_device *pdev,
+          u32 fps_level);
 };
 
 /*===========================================================================
@@ -193,6 +195,8 @@ struct platform_device *msm_fb_device_alloc(struct msm_fb_panel_data *pdata,
 						u32 type, u32 id);
 int panel_next_on(struct platform_device *pdev);
 int panel_next_off(struct platform_device *pdev);
+int panel_next_fps_level_change(struct platform_device *pdev,
+          u32 fps_level);
 
 int lcdc_device_register(struct msm_panel_info *pinfo);
 
