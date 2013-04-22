0242b0344ea3fa5bc2599123210603aecdcc343f
aznrice2k4 <aznrice2k4@gmail.com>
2013-04-22 01:04:02 -0500

    Low CPU Clocks: Allow ultra low CPU frequencies for better battery performance - originally commited by faux123 but modified for our kernel

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -124,6 +124,12 @@ static void set_acpuclk_L2_freq_foot_print(unsigned khz)
 #define SEC_SRC_SEL_QSB		0
 #define SEC_SRC_SEL_AUX		2
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE    36
+#else
+#define FREQ_TABLE_SIZE    35
+#endif
+
 /* HFPLL registers offsets. */
 #define HFPLL_MODE		0x00
 #define HFPLL_CONFIG_CTL	0x04
@@ -544,6 +550,9 @@ static struct l2_level l2_freq_tbl_8960_kraitv2[] = {
 
 static struct acpu_level acpu_freq_tbl_8960_kraitv2_slow[] = {
 	{ 0, { STBY_KHZ, QSB,   0, 0, 0x00 }, L2(0),   850000 },
+#ifdef CONFIG_LOW_CPUCLOCKS
+  { 1, {   162000, HFPLL, 2, 0, 0x0C }, L2(0),   900000 },
+#endif
 	{ 1, {   192000, PLL_8, 0, 2, 0x00 }, L2(1),   900000 },
 	{ 1, {   384000, HFPLL, 2, 0, 0x20 }, L2(7),   900000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(7),   950000 },
@@ -571,6 +580,9 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_slow[] = {
 
 static struct acpu_level acpu_freq_tbl_8960_kraitv2_nom[] = {
 	{ 0, { STBY_KHZ, QSB,   0, 0, 0x00 }, L2(0),   800000 },
+#ifdef CONFIG_LOW_CPUCLOCKS
+	{ 1, {   162000, HFPLL, 2 , 0, 0x0C }, L2(0),   800000 },
+#endif
 	{ 1, {   192000, PLL_8, 0, 2, 0x00 }, L2(1),   800000 },
 	{ 1, {   384000, HFPLL, 2, 0, 0x20 }, L2(7),   850000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(7),   900000 },
@@ -598,6 +610,9 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_nom[] = {
 
 static struct acpu_level acpu_freq_tbl_8960_kraitv2_fast[] = {
 	{ 0, { STBY_KHZ, QSB,   0, 0, 0x00 }, L2(0),   800000 },
+#ifdef CONFIG_LOW_CPUCLOCKS
+	{ 1, {   162000, HFPLL, 2, 0, 0x0C }, L2(0),   800000 },
+#endif
 	{ 1, {   192000, PLL_8, 0, 2, 0x00 }, L2(1),   800000 },
 	{ 1, {   384000, HFPLL, 2, 0, 0x20 }, L2(7),   800000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(7),   850000 },
